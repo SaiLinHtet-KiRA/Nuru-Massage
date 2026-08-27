1afa1ce9ae25eb4519f55078b8561c687c8bf87c
SAILIN HTET <sailinhtet.d@gmail.com>
2026-08-27 17:53:12 +0700

    change images

--- components/layout/ImageGrid.tsx
+++ components/layout/ImageGrid.tsx
@@ -6,22 +6,32 @@ import { imageVariants } from "@/style/FeedIn";
 export default function ImageGrid() {
   return (
     <section
-      className="w-screen md:h-svh h-auto min-h-svh grid md:grid-rows-2 md:grid-cols-5 grid-cols-1 auto-rows-auto md:gap-0 gap-2"
+      className="relative w-screen md:h-svh h-auto min-h-svh "
       id="gallery"
     >
-      {Images.map((src, i) => (
-        <MotionImage
-          variants={imageVariants}
-          key={"image-" + i}
-          initial="offscreen"
-          whileInView="onscreen"
-          width={1080}
-          height={960}
-          src={src}
-          alt="M&M Massage room photo"
-          className="w-[80svw] md:h-full aspect-[4/6] object-cover object-top md:rounded-none rounded-xl mx-auto"
-        />
-      ))}
+      <MotionImage
+        width={1080}
+        height={960}
+        src="/background.webp"
+        alt=""
+        loading="eager"
+        className="absolute w-screen h-svh top-0 md:object-cover object-fill object-right -z-1"
+      />
+      <div className="md:pt-0 pt-[12svh] h-svh overflow-scroll grid md:grid-rows-2 md:grid-cols-5 grid-cols-1 auto-rows-auto md:gap-0 gap-2">
+        {Images.map((src, i) => (
+          <MotionImage
+            variants={imageVariants}
+            key={"image-" + i}
+            initial="offscreen"
+            whileInView="onscreen"
+            width={1080}
+            height={960}
+            src={src}
+            alt="M&M Massage room photo"
+            className="w-[80svw] md:h-full aspect-[4/6] object-cover object-top md:rounded-none rounded-xl mx-auto"
+          />
+        ))}
+      </div>
     </section>
   );
 }
